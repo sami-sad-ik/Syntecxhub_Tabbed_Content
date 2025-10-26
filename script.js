3c0f03b72e52b4a616c71f82525e57c2120cf73f
sami-sad-ik <sadiksami530@gmail.com>
2025-10-26 22:06:36 +0600

    tab 1 is selected by default

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const buttons = document.querySelectorAll(".tab_btn");
 const contents = document.querySelectorAll(".tab_content");
 
 buttons.forEach((button, idx) => {
+  buttons[0].classList.add("btn_active");
+  contents[0].classList.add("content_active");
   button.addEventListener("click", () => {
     buttons.forEach((btn) => btn.classList.remove("btn_active"));
     contents.forEach((content) => content.classList.remove("content_active"));
